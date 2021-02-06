9f1c646bc41bd7108c8db603a76517461451ba42
Wolf-Dev10 <78599088+Wolf-Dev10@users.noreply.github.com>
2021-02-06 08:59:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('ODA3NDgzNzQ4NTk3MzY2ODQ1.YB4p1w.hRmuv0LLfZW_5oINNJlCfEee-1s');
+client.login('ODA3NDgzNzQ4NTk3MzY2ODQ1.YB4p1w.hpkrXtk8E9bbchUffvU9o3dWeJI');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
