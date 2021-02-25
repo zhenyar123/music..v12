9f955154e298f7a787d959c5c30c1cfb85f1d7fe
dev-calli721 <77979700+dev-calli721@users.noreply.github.com>
2021-02-25 19:03:15 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -1,24 +1,25 @@
-const { MessageEmbed } = require("discord.js");
+const { Client, Collection, MessageEmbed } = require(`discord.js`);
+const { 
+  PREFIX, 
+} = require(`../config.json`);
 
-  const prefix = ("c!");
   
+
 
 module.exports = {
   name: "help",
   aliases: ["h"],
-  description: "Display all commands and descriptions",
-  cooldown: 3,
-  edesc: "Type help to get a short preview of all Commands, Type help <COMMANDNAME> to get extended information about this one command!",
+  cooldown: 8,
+  description: "**all commands**",
   execute(message) {
     let commands = message.client.commands.array();
-     
-    let helpEmbed = new MessageEmbed()
-    .setFooter(message.author.username, message.author.displayAvatarURL)
-    .setColor('83c0ff')
-    .setTitle("Zooya Help")
-  .setDescription(`
-[Support](https://discord.gg/CHUKd9z87E) - [Invite](https://discord.com/api/oauth2/authorize?client_id=803650426570014730&permissions=8&scope=bot)
 
+    let helpEmbed = new MessageEmbed()
+    .setTitle(`Zooya Help`)
+    .setThumbnail(message.author.avatarURL())
+    .setDescription(`
+
+[Support](https://discord.gg/CHUKd9z87E) - [Invite](https://discord.com/api/oauth2/authorize?client_id=803650426570014730&permissions=8&scope=bot)
 **user Commands**
 c!prefix - c!uptime - c!ping - c!invite
 **Music Commands**
@@ -27,12 +28,15 @@ c!resume - c!search - c!volume - c!skipto- c!shuffle - c!remove - c!lyrics - c!l
 **Filter Commands**
 c!filter bassboost - c!filter 8D - c!filter vaporwave - c!filter nightcore - c!filter phaser - c!filter tremolo - c!filter vibrato - c!filter surrounding - c!filter pulsator - c!filter subboost - c!filter clear
 
+
+
 `)
-    
-       
 
-    helpEmbed.setTimestamp();
-
+  
+   .setColor("83c0ff");
+   helpEmbed.setTimestamp();
+   message.react("<:emoji_48:814468558871461938>")
     return message.channel.send(helpEmbed).catch(console.error);
+
   }
 };
