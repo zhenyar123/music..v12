78d15d074e291f02474605c60a54d50d7ece83f7
dev-calli721 <77979700+dev-calli721@users.noreply.github.com>
2021-02-25 19:16:43 +0300

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -15,17 +15,11 @@ module.exports = {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
-    .setTitle(`Zooya Help`)
-    .setThumbnail(message.author.avatarURL())
     .setDescription(`
-[Support](https://discord.gg/CHUKd9z87E) - [Invite](https://discord.com/api/oauth2/authorize?client_id=803650426570014730&permissions=8&scope=bot)
-**user Commands**
-c!prefix - c!uptime - c!ping - c!invite
-**Music Commands**
-c!play - c!pause- c!skip - c!queue
-c!resume - c!search - c!volume - c!skipto- c!shuffle - c!remove - c!lyrics - c!loop 
-**Filter Commands**
-c!filter bassboost - c!filter 8D - c!filter vaporwave - c!filter nightcore - c!filter phaser - c!filter tremolo - c!filter vibrato - c!filter surrounding - c!filter pulsator - c!filter subboost - c!filter clear
+
+<:emoji_63:814468559823831050> [Invite](https://discord.com/api/oauth2/authorize?client_id=803650426570014730&permissions=8&scope=bot)
+
+<:emoji_63:814468559823831050> [Support](https://discord.gg/CHUKd9z87E)
 `)
 
   
