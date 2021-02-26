44d2c69e0122628b5e83a411872bbebae76e6b2b
dev-calli721 <77979700+dev-calli721@users.noreply.github.com>
2021-02-26 14:08:02 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -15,19 +15,37 @@ module.exports = {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
-    .setTitle(`Zooya Help`)
+    .setTitle(`<:emoji_62:814468559613722634> | **Zooya**`)
     .setThumbnail(message.author.avatarURL())
     .setDescription(`
 
-[Support](https://discord.gg/CHUKd9z87E) - [Invite](https://discord.com/api/oauth2/authorize?client_id=803650426570014730&permissions=8&scope=bot)
-**user Commands**
-c!prefix - c!uptime - c!ping - c!invite - c!support
-**Music Commands**
-c!play - c!pause- c!skip - c!queue
-c!resume - c!search - c!volume - c!skipto- c!shuffle - c!remove - c!lyrics - c!loop 
-**Filter Commands**
-c!filter bassboost - c!filter 8D - c!filter vaporwave - c!filter nightcore - c!filter phaser - c!filter tremolo - c!filter vibrato - c!filter surrounding - c!filter pulsator - c!filter subboost - c!filter clear
+<:emoji_61:814468559597207572> c!play
+<:emoji_61:814468559597207572> c!skip
+<:emoji_61:814468559597207572> c!skipto
+<:emoji_61:814468559597207572> c!stop
+<:emoji_61:814468559597207572> c!volume
+<:emoji_61:814468559597207572> c!nowplaying
+<:emoji_61:814468559597207572> c!suffle
+<:emoji_61:814468559597207572> c!search
+<:emoji_61:814468559597207572> c!resume
+<:emoji_61:814468559597207572> c!remove
+<:emoji_61:814468559597207572> c!queue
+<:emoji_61:814468559597207572> c!pause
+<:emoji_61:814468559597207572> c!filter
+<:emoji_61:814468559597207572> c!loop
+<:emoji_61:814468559597207572> c!lyric
+<:emoji_61:814468559597207572> c!radio
+=================
+<:emoji_61:814468559597207572> C!invite
+<:emoji_61:814468559597207572> c!support
+<:emoji_61:814468559597207572> c!about
+<:emoji_61:814468559597207572> c!help
+<:emoji_61:814468559597207572> c!uptime
+<:emoji_61:814468559597207572> c!ping
+=================
+<:emoji_63:814468559823831050> Links
 
+[Support](https://discord.gg/CHUKd9z87E) - [Invite](https://discord.com/api/oauth2/authorize?client_id=803650426570014730&permissions=8&scope=bot)
 
 
 `)
