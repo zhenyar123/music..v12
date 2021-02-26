c4456d541d6b9eae1eabb7ed06e22ba31b4f7e57
dev-calli721 <77979700+dev-calli721@users.noreply.github.com>
2021-02-26 11:49:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('ODAzNjUwNDI2NTcwMDE0NzMw.YBA3yA.pKGd1IRiKbw8h8xxIRzqADvSuak');
+client.login('ODAzNjUwNDI2NTcwMDE0NzMw.YBA3yA.SFu73KXGuidVgNx5xPuIhET4H4U');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
