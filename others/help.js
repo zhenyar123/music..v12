fb2a3ea8b4422476472b859a6e3ba587d89b8d68
dev-calli721 <77979700+dev-calli721@users.noreply.github.com>
2021-02-26 16:41:19 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -18,7 +18,6 @@ module.exports = {
     .setThumbnail(`https://media.discordapp.net/attachments/809000331807424512/814581827192881152/image0.png `)
     .setTitle(`**Zooya Help**`)
     .setDescription(`
-[Support](https://discord.gg/CHUKd9z87E) - [Invite](https://discord.com/api/oauth2/authorize?client_id=803650426570014730&permissions=8&scope=bot)
 
  **User Commands**
 \`c!invite\` - \`c!support\` - \`c!about\`
@@ -29,15 +28,11 @@ module.exports = {
 \`c!suffle\` - \`c!search\` - \`c!resume\`
 \`c!remove\` - \`c!queue\` - \`c!filter\`
 \`c!loop\` - \`c!lyric\` - \`c!radio\`
-**Filter Commands**
-\`c!fi bassboost\` - \`c!fi 8D\` - \`c!fi vaporwave\`
-\`c!fi nightcore\` - \`c!fi phaser\` - \`c!fi tremolo\`
-\`c!fi vibrato\` - \`c!fi surrounding\` - \`c!fi pulsator\`
-\`c!fi subboost\` - \`c!fi clear\`
-
+**Links**
+[Support](https://discord.gg/CHUKd9z87E) - [Invite](https://discord.com/api/oauth2/authorize?client_id=803650426570014730&permissions=8&scope=bot)
 `)
 
-   .setFooter(`Requested | ${message.author.tag}`, message.author.avatarURL())
+   .setFooter(`${message.author.tag}`, message.author.avatarURL())
    .setColor("83c0ff");
    message.react("<:emoji_48:814468558871461938>")
     return message.channel.send(helpEmbed).catch(console.error);
