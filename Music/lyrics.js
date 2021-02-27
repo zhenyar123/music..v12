fc287ee224c350c621cffe7ecbeee24da1432be5
dev-calli721 <77979700+dev-calli721@users.noreply.github.com>
2021-02-27 19:38:01 +0300

    Update lyrics.js

--- Music/lyrics.js
+++ Music/lyrics.js
@@ -20,7 +20,7 @@ async execute(message) {
     //if not in a Guild return
     if(!message.guild) return;
     //react with approve emoji
-    message.react("<:emoji_48:814468558871461938>").catch(console.error);
+    message.react("<:emoji_74:815251307635015731>").catch(console.error);
     //Get the current Queue
     const queue = message.client.queue.get(message.guild.id);
     //If no Queue Error
@@ -48,9 +48,9 @@ async execute(message) {
     }
     //define lyrics Embed
     let lyricsEmbed = new MessageEmbed()
-      .setTitle("<:emoji_62:814468559613722634> Lyrics")
+      .setTitle("<:emoji_73:815251306268328000> Lyrics")
       .setDescription(lyrics)
-      .setColor("83c0ff")
+      .setColor("YELLOW")
     //if to long make slice it
     if (lyricsEmbed.description.length >= 2048)
       //slice the embed description and redefine it
