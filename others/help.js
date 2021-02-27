ae041726fe350c9cee2fb7bc04dc09cbeb5f76d4
dev-calli721 <77979700+dev-calli721@users.noreply.github.com>
2021-02-27 20:28:23 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -15,14 +15,14 @@ module.exports = {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
-    .setThumbnail(`https://media.discordapp.net/attachments/809000331807424512/814581827192881152/image0.png `)
+    .setThumbnail(`https://media.discordapp.net/attachments/815252825012568085/815266512414703656/image0.png `)
     .setDescription(`
 
-<:emoji_60:814468559475310604> **User Commands**
+**User Commands**
 \`c!invite\` - \`c!support\` - \`c!about\`
 \`c!ping\` - \`c!prefix\` - \`c!uptime\`
 
-<:emoji_61:814468559597207572> **Music Commands**
+**Music Commands**
 \`c!play\` - \`c!skip\` - \`c!skipto\`
 \`c!stop\` - \`c!volume\` - \`nowplaying\`
 \`c!suffle\` - \`c!search\` - \`c!resume\`
@@ -30,16 +30,12 @@ module.exports = {
 \`c!loop\` - \`c!lyric\` - \`c!radio\`
 
 <:emoji_63:814468559823831050> **Links**
-[Click Here](https://discord.gg/CHUKd9z87E)
-\`Support Zooya\`
--
-[Click Here](https://discord.com/api/oauth2/authorize?client_id=803650426570014730&permissions=8&scope=bot)
-\`Invite Zooya\`
+[Support](https://discord.gg/CHUKd9z87E) - [Invite](https://discord.com/api/oauth2/authorize?client_id=803650426570014730&permissions=8&scope=bot)
 `)
 
    .setFooter(`${message.author.tag}`, message.author.avatarURL())
-   .setColor("83c0ff");
-   message.react("<:emoji_48:814468558871461938>")
+   .setColor("YELLOW");
+   message.react("<:emoji_74:815251307635015731>")
     return message.channel.send(helpEmbed).catch(console.error);
 
   }
