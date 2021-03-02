034c35e1c85049bde5f9c6a76ce2e59571687015
dev-calli721 <77979700+dev-calli721@users.noreply.github.com>
2021-03-02 12:19:45 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -20,18 +20,18 @@ module.exports = {
     .setDescription(`
 
 **User Commands**
-\`c!invite\` - \`c!support\` - \`c!about\`
-\`c!ping\` - \`c!prefix\` - \`c!uptime\`
+\`c?invite\` - \`c?support\` - \`c?about\`
+\`c?ping\` - \`c?prefix\` - \`c?uptime\`
 
 **Music Commands**
-\`c!play\` - \`c!skip\` - \`c!skipto\`
-\`c!stop\` - \`c!volume\` - \`nowplaying\`
-\`c!shuffle\` - \`c!search\` - \`c!resume\`
-\`c!remove\` - \`c!queue\` - \`c!filter\`
-\`c!loop\` - \`c!lyrics\` - \`c!radio\`
+\`c?play\` - \`c?skip\` - \`c?skipto\`
+\`c?stop\` - \`c?volume\` - \`c?nowplaying\`
+\`c?shuffle\` - \`c?search\` - \`c?resume\`
+\`c?remove\` - \`c?queue\` - \`c?filter\`
+\`c?loop\` - \`c?lyrics\` - \`c?radio\`
 
 **Links**
-[Support](https://discord.gg/CHUKd9z87E) - [Invite](https://discord.com/api/oauth2/authorize?client_id=797017110776643645&permissions=3758096369&scope=bot)
+[Support](https://discord.gg/5aENC4fNVj) - [Invite](https://discord.com/api/oauth2/authorize?client_id=803965149941071875&permissions=8&scope=bot)
 `)
 
    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
