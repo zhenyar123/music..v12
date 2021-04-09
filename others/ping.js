a7b8b0089cad7aa3bae3df309fcbb85894b4f47e
zhenyar123 <70575666+zhenyar123@users.noreply.github.com>
2021-04-09 23:19:26 +0300

    Update ping.js

--- others/ping.js
+++ others/ping.js
@@ -13,7 +13,7 @@ module.exports = {
   edesc: "Type this command to see how fast the Bot can response to your messages / commands inputs!",
   execute(message, args, client) {
     //react with approve emoji
-    message.react("");
+    message.react("<:ping:830174810297860156>");
     //send the Ping embed
     message.reply(new MessageEmbed().setColor("YELLOW").setTitle(" `" + client.ws.ping + "ms`"));
   }
