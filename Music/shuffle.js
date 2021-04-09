657c69da26c5ce14c76cb67afc27dc07d5111b42
zhenyar123 <70575666+zhenyar123@users.noreply.github.com>
2021-04-09 21:50:19 +0300

    Update shuffle.js

--- Music/shuffle.js
+++ Music/shuffle.js
@@ -19,7 +19,7 @@ execute(message,args,client) {
     //if not in a guild return
     if(!message.guild) return;
     //react with approve emoji
-    message.react("<:emoji_74:815251307635015731>").catch(console.error);
+    message.react("").catch(console.error);
     //get the Queue
     const queue = message.client.queue.get(message.guild.id);
     //if no queue return error
