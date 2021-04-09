334dc289a8eb87715b55e1f72e67c6f780c3eb52
zhenyar123 <70575666+zhenyar123@users.noreply.github.com>
2021-04-09 23:24:20 +0300

    Update uptime.js

--- others/uptime.js
+++ others/uptime.js
@@ -17,7 +17,7 @@ module.exports = {
     let minutes = Math.floor(client.uptime / 60000) % 60;
     let seconds = Math.floor(client.uptime / 1000) % 60;
     //react with approve emoji
-    message.react("<:emoji_74:815251307635015731>");
+    message.react("<:setting:830141142687285298>");
     return message.channel.send(new MessageEmbed().setColor("YELLOW").setTitle(`<:emoji_74:815251307635015731>\`Zooya Uptime\`\n\n\`${days}d\` \`${hours}h\` \`${minutes}m\` \`${seconds}s\n\``));
 
 
