42af35412003f956a4c1aeccd8ee6abe867d5ead
zhenyar123 <70575666+zhenyar123@users.noreply.github.com>
2021-04-09 21:51:31 +0300

    Update skip.js

--- Music/skip.js
+++ Music/skip.js
@@ -19,7 +19,7 @@ execute(message) {
     //if not in a guild retunr
     if (!message.guild) return;
     //react with approve emoji
-    message.react("<:emoji_74:815251307635015731>").catch(console.error);
+    message.react("").catch(console.error);
     //get the queue
     const queue = message.client.queue.get(message.guild.id);
     //if no Queue return error
