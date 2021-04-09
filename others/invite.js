925302c77bd17507bb549d4cf0f3ecf75d0dfddd
zhenyar123 <70575666+zhenyar123@users.noreply.github.com>
2021-04-09 22:10:14 +0300

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -17,12 +17,12 @@ module.exports = {
     let helpEmbed = new MessageEmbed()
     .setDescription(`
 
-<:emoji_65:815251304070905857> [Click here](https://discord.com/api/oauth2/authorize?client_id=803965149941071875&permissions=8&scope=bot) **to invite the bot.**
+<:all:830157072116351026>  [Click here](https://discord.com/api/oauth2/authorize?client_id=830137375010979870&permissions=8&scope=bot) **to invite the bot.**
 `)
 
   
    .setColor("YELLOW");
-   message.react("<:emoji_74:815251307635015731>")
+   message.react("<:all:830157072116351026>")
     return message.channel.send(helpEmbed).catch(console.error);
 
   }
